365086781ead0f3a8fb98a1cedb048a784891f32
maggi brisbin <margaret.marsbrisbin@oist.jp>
2023-11-18 21:42:44 -0500

    added data files

---
+++ legendplot.R
@@ -0,0 +1,8 @@
+#Legend Function
+
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  legend
+}
